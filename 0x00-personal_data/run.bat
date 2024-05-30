244729258cfc6515967b5b4099bea8bcef7e5fa8
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-30 22:35:26 +0100

    Connect to database to read a users table1

--- 0x00-personal_data/run.bat
+++ 0x00-personal_data/run.bat
@@ -1,6 +1,3 @@
 @echo off
-set PERSONAL_DATA_DB_USERNAME=root
-set PERSONAL_DATA_DB_PASSWORD=root
-set PERSONAL_DATA_DB_HOST=localhost
-set PERSONAL_DATA_DB_NAME=my_db
-python 3-main.py
+mysql -uroot -p < 3-main.sql
+pause
